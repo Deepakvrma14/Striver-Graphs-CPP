bc421066728fc21a1301341c0389bc9e7c428dcd
Deepakvrma14 <deepakverma1482003@gmail.com>
2023-10-06 22:59:26 +0530

    dfs bfs both included

--- Part-2 Problems on BFS, DFS/L8 Number of Islands/L7 Number of Provinces/785-Is-Graph-Bipartite.cpp
+++ Part-2 Problems on BFS, DFS/L8 Number of Islands/L7 Number of Provinces/785-Is-Graph-Bipartite.cpp
@@ -13,14 +13,14 @@ public:
             if(color[i] == -1){
                 
                 //* Method-1 -> BFS
-                // if(bfs(i, graph, color, n) == false){
-                //     return false;
-                // }
-
-                //* Method-2 -> DFS
-                if(dfs(i, 0, graph, color) == false){
+                if(bfs(i, graph, color, n) == false){
                     return false;
                 }
+
+                //* Method-2 -> DFS
+                // if(dfs(i, 0, graph, color) == false){
+                //     return false;
+                // }
             }
         }
 
