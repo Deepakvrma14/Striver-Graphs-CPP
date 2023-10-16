1e77378ecbdf34b113f907a0762f24e553e1542d
Deepakvrma14 <deepakverma1482003@gmail.com>
2023-10-17 00:19:36 +0530

    modified abc.cpp added tasks.json

--- abc.cpp
+++ abc.cpp
@@ -5,44 +5,44 @@
 
 using namespace std;
 
-int getValue(string s) {
-    int value = 0;
-    for (char c : s) {
-        if (isdigit(c)) {
-            value = value * 10 + (c - '0');
-        }
-    }
-    return (value == 0) ? s.length() : value;
-}
+// int getValue(string s) {
+//     int value = 0;
+//     for (char c : s) {
+//         if (isdigit(c)) {
+//             value = value * 10 + (c - '0');
+//         }
+//     }
+//     return (value == 0) ? s.length() : value;
+// }
 
-long long maxProductOfValues(vector<string> arr) {
-    int n = arr.size();
-    long long max1 = 0, max2 = 0;
+// long long maxProductOfValues(vector<string> arr) {
+//     int n = arr.size();
+//     long long max1 = 0, max2 = 0;
 
-    for (int i = 0; i < n; i++) {
-        int value = getValue(arr[i]);
-        if (value > max1) {
-            max2 = max1;
-            max1 = value;
-        } else if (value > max2) {
-            max2 = value;
-        }
-    }
+//     for (int i = 0; i < n; i++) {
+//         int value = getValue(arr[i]);
+//         if (value > max1) {
+//             max2 = max1;
+//             max1 = value;
+//         } else if (value > max2) {
+//             max2 = value;
+//         }
+//     }
 
-    return max1 * max2;
-}
+//     return max1 * max2;
+// }
 
 int main() {
     int n;
     cin >> n;
-    vector<string> arr(n);
+    // vector<string> arr(n);
     
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
-    }
+    // for (int i = 0; i < n; i++) {
+    //     cin >> arr[i];
+    // }
 
-    long long result = maxProductOfValues(arr);
-    cout << result << endl;
+    // long long result = maxProductOfValues(arr);
+    cout << n << endl;
     
     return 0;
 }
